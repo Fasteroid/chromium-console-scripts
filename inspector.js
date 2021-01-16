001dd707c3ac3d4c90dd19b2f9f8d652df6f7533
Fasteroid <fasteroid@hotmail.com>
2021-01-15 20:14:30 -0500

    minification, bugfixes, and new features
    added min depth and an option to not detour constructors (since that seems to break stuff)

--- inspector.js
+++ inspector.js
@@ -2,115 +2,93 @@
 *  Fast's JS Function Call Inspector: *
 *        Chrome Console Edition       *
 **************************************/
-var SPEC = [];
+var spec = [];
 
 /*  HOW TO USE:
- *  Paste into chrome console and smash enter to gain  O M N I P R E S E N C E
+ *  Paste into chrome console and smash enter to gain  T O T A L   O M N I P R E S E N C E!
+ *  If total omnipresence breaks the website, try individually detouring elements instead.
+ *  MIN_SEARCH_DEPTH is useful for not detouring root functions
 */
-SPEC.EXPLORATION_BLACKLIST = ["SPEC","traversalMarker","$","jQuery","Ajax","console"]; 
-SPEC.MAX_SEARCH_DEPTH      = 10;
-SPEC.DETOUR_NATIVE_FUNCS   = false;
+spec.EXPLORATION_BLACKLIST = ["spec","$","jQuery","Ajax","React","angular"]; 
+spec.FUNCTION_BLACKLIST    = ["log"];
+spec.MAX_SEARCH_DEPTH      = 6;
+spec.MIN_SEARCH_DEPTH      = 1;
+spec.DETOUR_NATIVE_FUNCS   = false;
+spec.ANTISPAM_INTERVAL     = 0.1;
 
-SPEC.shouldExplore = function(val) {
-    try { 
-        if (val === undefined || val === null) { return false }
-        if (val + "" == "[object Window]") { return false; }
-        return typeof val === 'object';
-    }
-    catch (e) {
-        return false;
-    }
-}
+// helper functions don't touch
+spec.shouldExplore = function(val) { try {  if (val === undefined || val === null) { return false } if (val + "" == "[object Window]") { return false; } return typeof val === 'object'; } catch (e) { return false; } }
+spec.doAntispam = spec.ANTISPAM_INTERVAL > 0;
+if( spec.doAntispam ){ spec.loggable = false; spec.antispam = function(){ spec.loggable = true; setTimeout(spec.antispam, spec.ANTISPAM_INTERVAL) }; spec.antispam(); }
+spec.join = function(array){ var output = []; for (let index = 0; index < array.length; index++) { if( array[index] != null ){ if( typeof array[index] === 'string' ){ output[index] = "'" + array[index] + "'"; } else{ output[index] = array[index]; } } else{ output[index] = "null"} } return output.join(", "); }
+spec.isCustom = function(f){ return !(/\{\s*\[native code\]\s*\}/).test(f); }
+spec.or = function(a,b){ if( a != "" ){ return a; } return b; }
+spec.trace = function(){ let trace = (new Error().stack).replaceAll("    ","  ").split("at ").splice(2); var output = []; for (let index = 0; index < trace.length; index++) { output[index] = " ".repeat(index) + trace[index]; } return output.join(""); }
+spec.construct = function Arguments( args ) { for (let index = 0; index < args.length; index++) { this[index] = args[index]; } }
+spec.traversed = { }
+spec.log = "";
 
-SPEC.format = function(path,value){
-    if( value.length > SPEC.NEWLINE_AT*0.7 || value.search("\n") != -1 ){
-        path = path + " = \n" + value + "\n" + SPEC.SEPARATOR
-        if( !SPEC.lastseparated ){
-            SPEC.lastseparated = true;
-            path = SPEC.SEPARATOR + path;
-        }
-        return path;
-    }
-    else if( path.length > SPEC.NEWLINE_AT ){
-        return path + "\t".repeat(Math.max(0,Math.ceil((SPEC.NEWLINE_AT-path.length)/4))) + "= \n" + value + "\n"
-    }
-    SPEC.lastseparated = false;
-    return path + "\t".repeat(Math.max(0,Math.ceil((SPEC.NEWLINE_AT-path.length)/4))) + "= " + value + "\n"
-}
-
-SPEC.join = function(array){
-    var output = [];
-    for (let index = 0; index < array.length; index++) {
-        if( array[index] != null ){ // NO NULL!!!1
-            if( typeof array[index] === 'string' ){
-                output[index] = "'" + array[index] + "'";
-            }
-            else{
-                output[index] = array[index];
-            }
-        }
-        else{
-            output[index] = "null"
-        }
-    }
-    return output.join(", ");
-}
-
-SPEC.isCustom = function(f){ return !(/\{\s*\[native code\]\s*\}/).test(f); }
-SPEC.or = function(a,b){ if( a != "" ){ return a; } return b; }
-SPEC.trace = function(){ let trace = (new Error().stack).replaceAll("    ","  ").split("at ").splice(2); var output = []; for (let index = 0; index < trace.length; index++) { output[index] = " ".repeat(index) + trace[index]; } return output.join(""); }
-SPEC.construct = function Arguments( args ) { for (let index = 0; index < args.length; index++) { this[index] = args[index]; } }
-
-SPEC.seed = Math.random(Date.parse());
-console.log("seed: "+SPEC.seed);
-SPEC.log = "";
-SPEC.locations = { }
-
-SPEC.recursivelyExplore = function (obj, path, depth) {    
-
-    obj.traversalMarker = SPEC.seed;
+spec.detour = function (obj, path, depth) {    
+    if( depth == undefined ){ depth = 0 };
+    if( spec.traversed[path] ){ return; }
+    if( depth > spec.MAX_SEARCH_DEPTH ) { return; }
+    spec.traversed[path] = true;
     for (const key in obj) {
-
-        if( SPEC.EXPLORATION_BLACKLIST.includes(key) ){ continue; }
         if( !obj.hasOwnProperty(key) ) { continue; }
         var obj2 = obj[key];
-
-        if( SPEC.shouldExplore(obj2) ){
-            if (depth < SPEC.MAX_SEARCH_DEPTH) {
-                try{ 
-                    SPEC.recursivelyExplore(obj2, path + "." + key, depth + 1); 
-                } 
-                catch (e){ }
-            }
+        if( spec.shouldExplore(obj2) ){
+            if( spec.EXPLORATION_BLACKLIST.includes(key) ){ continue; }
+            try{ 
+                spec.detour(obj2, path + "." + key, depth + 1); 
+            } 
+            catch (e){ }
         }
-        else if( obj2!==null && typeof obj2 === 'function' && !obj2.__IsWrapped && ( SPEC.DETOUR_NATIVE_FUNCS || SPEC.isCustom(obj2) ) ){
-            if ( SPEC.log.length > 8192 ) {
-                console.log(SPEC.log);
-                SPEC.log = "";
+        else if( depth >= spec.MIN_SEARCH_DEPTH && obj2!==null && obj2!=undefined && typeof obj2 === 'function' && !obj2.__IsWrapped && ( spec.DETOUR_NATIVE_FUNCS || spec.isCustom(obj2) ) ){
+            if( spec.FUNCTION_BLACKLIST.includes(key) ){ continue; }
+            if ( spec.log.length > 8192 ) {
+                console.log(spec.log);
+                spec.log = "";
             }
             // janky hack mate, thanks
             // https://traceoverflow.com/questions/9134686/adding-code-to-a-javascript-function-programmatically
             obj[key] = (function() {
                 var cached_function = obj2;
-                SPEC.locations[cached_function] = path+"."+key;
-                return function() {
-
-                    console.groupCollapsed(SPEC.or(cached_function.name,"anonymous") + "(" + SPEC.join([...arguments]) + ");");
-                        console.log(new SPEC.construct([...arguments]));
-                        console.log(cached_function);
-                        console.log("Stack Trace:\n" + SPEC.trace());
-                        console.log("Location:\n" + SPEC.locations[cached_function]);
-                    console.groupEnd();
-
-                    var result = cached_function.apply(this, arguments); // use .apply() to call it
-                    return result;
-                };
+                cached_function.override = cached_function;
+                cached_function.__IsWrapped = true;
+                cached_function.__Location  = path+"."+key;
+                cached_function.__Path = path+".";
+                var new_function = {[cached_function.name]: function() { // new wrapped function
+                    if( spec.loggable ){
+                        spec.loggable = !spec.doAntispam;
+                        try{
+                            console.groupCollapsed(cached_function.__Path + spec.or(cached_function.name,"<anonymous>") + "(" + spec.join([...arguments]) + ");");
+                            console.log(new spec.construct([...arguments]));
+                            console.log(cached_function.override);
+                            console.log("Stack Trace:\n" + spec.trace());
+                            console.log("Location:\n" + cached_function.__Location);
+                            console.log("To Unhook:\n" + cached_function.__Location + " = " + cached_function.__Location + ".old;");
+                        }
+                        finally{
+                            console.groupEnd();
+                        }
+                    }
+                    var result = cached_function.override.apply(this, arguments); // use .apply() to call it
+                    if(result != undefined){
+                        return result;
+                    }
+                }}[cached_function.name];
+                new_function.name
+                new_function.__IsWrapped = true;
+                new_function.old  = cached_function;
+                spec.log = spec.log + "DETOURED FUNC: " + (obj2+"").split(")")[0] + "){...} @ "+cached_function.__Location + "\n";
+                return new_function;
             })();
-            SPEC.log = SPEC.log + "DETOURED FUNC: " + (obj2+"").split(")")[0] + "){...}\n";
-            obj[key].__IsWrapped = true;
         }
-
+    }
+    if( depth == 0 ){
+        console.log(spec.log);
+        spec.log = "";
     }
 }
-SPEC.recursivelyExplore(window, "window", 0);
-console.log(SPEC.log);
+
+spec.detour(window, "window", 0);
