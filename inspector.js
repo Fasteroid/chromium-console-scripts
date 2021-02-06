c2d40c3cb23f7647713e87548c7eb506aa526bf2
Fasteroid <fasteroid@hotmail.com>
2021-02-06 12:24:56 -0500

    fixed detourer missing some functions

--- inspector.js
+++ inspector.js
@@ -21,11 +21,13 @@ inspector.BANNED_KEYS           = ["inspector"]; // searching for functions will
 inspector.BANNED_FUNCTIONS      = ["log"];       // functions under these keys will be left alone
 inspector.MAX_SEARCH_DEPTH      = 6;             // max recursive depth to look for functions
 inspector.DETOUR_NATIVE_FUNCS   = false;         // detour [native code] ?
-inspector.burstLimit            = 64;
-inspector.burstTime             = 100; // in ms
+inspector.burstLimit            = 64;            // max messages per burstTime
+inspector.burstTime             = 100;           // in ms
 
-// helper functions
-inspector.shouldExplore = function(val) { try {  if (val === undefined || val === null) { return false } if (val + "" == "[object Window]") { return false; } return typeof val === 'object'; } catch (e) { return false; } } // avoid infinite recursion
+inspector.DETOUR_ALL_INSTANTLY  = false;         // run the function detourer immediately on the entire DOM?
+
+// helper functions don't touch
+inspector.shouldExplore = function(val) { try { if (val === undefined || val === null) { return false } if (val + "" == "[object Window]") { return false; } return typeof val === 'object'; } catch (e) { return false; } } // avoid infinite recursion
 inspector.join = function(array){ var output = []; for (let index = 0; index < array.length; index++) { if( array[index] != null ){ if( typeof array[index] === 'string' ){ output[index] = "'" + array[index] + "'"; } else{ output[index] = array[index]; } } else{ output[index] = "null"} } return output.join(", "); } // display null properly
 inspector.isCustom = function(f){ return !(/\{\s*\[native code\]\s*\}/).test(f); } // does it have native code?
 inspector.trace = function(){ let trace = (new Error().stack).replaceAll("    ","").split("at ").splice(3); var output = []; for (let index = 0; index < trace.length; index++) { output[index] = " ".repeat(index+1) + trace[index]; } return output.join(""); } // get fancy stack trace
@@ -35,7 +37,6 @@ inspector.originobj = function Origins( obj ) { this.function = obj }
 inspector.justify = function(string,value){ return string + "\t".repeat(Math.max(0,Math.ceil((value-string.length)/4))) } // aligns text
 inspector.resetAntiSpam = function(){ inspector.logs = 0; setTimeout( inspector.resetAntiSpam, inspector.burstTime ); }
 inspector.resetAntiSpam();
-
 inspector.handleLogging = function(data, args, result, exception){
     if( (inspector.burstLimit <= 0 || inspector.logs < inspector.burstLimit) ){
         inspector.logs++;
@@ -50,11 +51,12 @@ inspector.handleLogging = function(data, args, result, exception){
             console.log("Stack Trace:\n" + inspector.trace());
             if( !exception || exception.passed ){
                 console.log("To Unhook:\n" + data.location + " = " + data.location + ".old;");
+                console.log(data.function);
             }
             else{
+                console.log(data.function);
                 console.warn("Unhooked.")
             }
-            console.log(data.function);
         }
         finally{
             console.groupEnd();
@@ -72,7 +74,6 @@ inspector.detour = function (obj, root="", path=root, depth=0) {
     obj._inspector_breadcrumb = inspector.seed;
     try{
         for (const key in obj) {
-            if( !obj.hasOwnProperty(key) ) { continue; }
             var obj2 = obj[key];
             if( inspector.shouldExplore(obj2) ){
                 if( inspector.BANNED_KEYS.includes(key) ){ continue; }
@@ -84,13 +85,12 @@ inspector.detour = function (obj, root="", path=root, depth=0) {
                 ( !obj2._wrapped ) &&
                 ( inspector.DETOUR_NATIVE_FUNCS || inspector.isCustom(obj2) ) 
             ){
-
                 if( inspector.BANNED_FUNCTIONS.includes(key) ){ continue; }
                 if( !inspector.firstDetours[root] ){ 
                     console.groupCollapsed("Detoured Function List"); 
                     inspector.firstDetours[root] = true;
                 }
-                inspector.log[inspector.log.length] = inspector.justify( (obj2+"").split(")")[0] + "){...} ", 32) + " >\t\t"+ path+"."+key;                
+                inspector.log[inspector.log.length] = inspector.justify( (obj2+"").split(")")[0] + "){...} ", 64) + " >\t\t"+ path+"."+key;                
                 
                 // janky hack mate, thanks
                 // https://traceoverflow.com/questions/9134686/adding-code-to-a-javascript-function-programmatically
@@ -158,6 +158,10 @@ inspector.detour = function (obj, root="", path=root, depth=0) {
             }
         }
     }
+    catch(e){
+        console.log("Caught an exception while recursively detouring: ");
+        console.warn(e);
+    }
     finally{
         if( depth == 0 & inspector.firstDetours[root] ){
             console.log(inspector.log.join("\n"));
@@ -166,4 +170,7 @@ inspector.detour = function (obj, root="", path=root, depth=0) {
         }
     }
 }
-inspector.detour(window, "window");
+
+if(inspector.DETOUR_ALL_INSTANTLY){
+    inspector.detour(window, "window");
+}
