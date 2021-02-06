ed9f44971f0d81e07f19e29092e26ca9759330c7
Fasteroid <fasteroid@hotmail.com>
2021-02-06 15:02:15 -0500

    better logging and extended exploring

--- dumper.js
+++ dumper.js
@@ -2,7 +2,7 @@
 * Fast's JS Global Variable Dumper: *
 *      Chrome Console Edition       *
 ************************************/
-var dumper = { traversed: { } };
+var dumper = { log: [] };
 
 /*  HOW TO USE:
     Paste in chrome console and hit enter.  Take care if you set the max search depth really high.
@@ -12,6 +12,8 @@ dumper.MAX_SEARCH_DEPTH = 10;         // max recursive depth to search
 dumper.IGNORE_NULL      = true;       // if true, don't print keys that are null
 dumper.IGNORE_FUNC      = false;      // if true, don't print functions
 dumper.SEPARATOR        = "\n";
+dumper.GROUP_SIZE       = 134217728;
+dumper.EXPLORE_EVERYTHING = false;    // if true, explore inherited properties
 
 dumper.shouldExplore = function(val) {
     try { 
@@ -24,36 +26,22 @@ dumper.shouldExplore = function(val) {
     }
 }
 dumper.lastseparated = false;
-dumper.format = function(path,value){
-    if( value.length > 64 || value.search("\n") != -1 ){
-        path = path + " = \n" + value + "\n" + dumper.SEPARATOR
-        if( !dumper.lastseparated ){
-            dumper.lastseparated = true;
-            path = dumper.SEPARATOR + path;
-        }
-        return path;
-    }
-    else if( path.length > 90 ){
-        return path + "\t".repeat(Math.max(0,Math.ceil((96-path.length)/4))) + "= \n" + value + "\n"
-    }
-    dumper.lastseparated = false;
-    return path + "\t".repeat(Math.max(0,Math.ceil((96-path.length)/4))) + "= " + value + "\n"
-}
+dumper.justify = function(string,value){ return string + "\t".repeat(Math.max(0,Math.ceil((value-string.length)/4))) } // aligns text
 
-dumper.log = "";
 dumper.seed = Math.random(Date.parse());
 console.log("seed: "+dumper.seed);
 
 dumper.recursivelyExplore = function (obj, path, depth) {    
-    if( dumper.traversed[path] ){ return; }
+    if( obj.traversed == dumper.seed ){ return; }
     if( depth > dumper.MAX_SEARCH_DEPTH ) { return; }
     if( depth == 0 ){
         console.group("Global Key/Value Pairs:");
     }
-    dumper.traversed[path] = dumper.seed;
+    obj.traversed = dumper.seed;
     for (const key in obj) {
 
-        if( dumper.BANNED_KEYS.includes(key) ){ continue; }
+        if( !dumper.EXPLORE_EVERYTHING && !obj.hasOwnProperty(key) ) { continue; }
+        if( key=="traversed" || dumper.BANNED_KEYS.includes(key) ){ continue; }
         var obj2 = obj[key];
 
         if( dumper.shouldExplore(obj2) ){
@@ -61,34 +49,33 @@ dumper.recursivelyExplore = function (obj, path, depth) {
                 dumper.recursivelyExplore(obj2, path + "." + key, depth + 1); 
             } 
             catch (e){
-                dumper.log = dumper.log + dumper.format(path, "[Exploration Failed]");
+                dumper.log[dumper.log.length] = dumper.justify(path, 64) + "~ [Exploration Failed]\n";
             }
         }
-
         else if( ( !dumper.IGNORE_NULL || obj2!==null ) && ( !dumper.IGNORE_FUNC || typeof obj2 !== 'function' ) ){
             try {
                 var finalPath = path + "." + key
-                if ( dumper.log.length > 134217728 ) {
-                    console.log(dumper.log);
-                    dumper.log = "";
+                if ( dumper.log.length > dumper.GROUP_SIZE ) {
+                    console.log(dumper.log.join("\n"));
+                    dumper.log = [];
                 }
                 if( typeof obj2 === 'symbol' ){
-                    dumper.log = dumper.log + dumper.format(finalPath, "[Symbol] " + obj2.valueOf());
+                    dumper.log[dumper.log.length] = dumper.justify(finalPath, 64) + "= [Symbol] " + obj2.valueOf() + "\n";
                 }
                 else {
-                    dumper.log = dumper.log + dumper.format(finalPath, obj2+"");
+                    dumper.log[dumper.log.length] = dumper.justify(finalPath, 64) + "= " + obj2 + "\n";
                 }
             }
             catch (e) {
-                dumper.log = dumper.log + dumper.format(finalPath,"[Unprintable " + (typeof obj2) + "]");
+                dumper.log[dumper.log.length] = dumper.justify(finalPath, 64) + "~ [Unprintable " + (typeof obj2) + "]\n";
             }
         }
 
     }
     if( depth == 0 ){
-        console.log(dumper.log);
+        console.log(dumper.log.join("\n"));
         console.groupEnd();
-        dumper.log = "";
+        dumper.log = [];
     }
 }
 dumper.recursivelyExplore(window, "window", 0);
