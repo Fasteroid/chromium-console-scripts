b5f09de6e4fc55e85c405c6941d3500e1b695161
Fasteroid <fasteroid@hotmail.com>
2021-07-16 17:32:33 -0400

    rename this

--- chrome-dom-destroyer.js
+++ chrome-dom-destroyer.js
@@ -6,10 +6,10 @@
 // This is basically chrome-corrupter but less dangerous
 // You should be able to run this safely
 
-let DESTROYER = { VALUES_CACHE: {} };
+let DESTROYER = { VALUES_CACHE: {}, COUNT: 0 };
 
-DESTROYER.PROPERTIES_TO_CORRUPT = ["innerText","className","style","src","innerHTML"];
-DESTROYER.CORRUPT_CHANCE        = 0.5
+DESTROYER.PROPERTIES_TO_CORRUPT = ["className","style","src","fontFamily","innerText"];
+DESTROYER.CORRUPT_CHANCE        = 0.8
 
 for( property of DESTROYER.PROPERTIES_TO_CORRUPT ){
     DESTROYER.VALUES_CACHE[property] = ["lol"]; // prep tables
@@ -29,7 +29,11 @@ DESTROYER.readValues = function(node){
 DESTROYER.writeValues = function(node){
     for( property of DESTROYER.PROPERTIES_TO_CORRUPT ){
         if( Math.random() > DESTROYER.CORRUPT_CHANCE ){ return; }
-        node[property] = DESTROYER.pickRandom(DESTROYER.VALUES_CACHE[property])
+        try{
+            node[property] = DESTROYER.pickRandom(DESTROYER.VALUES_CACHE[property])
+            DESTROYER.COUNT++;
+        }
+        catch(e){}
     }
 }
 
@@ -48,3 +52,4 @@ DESTROYER.explore = function(node, action){
 DESTROYER.explore(document, DESTROYER.readValues);
 DESTROYER.explore(document, DESTROYER.writeValues);
 (new Audio("https://cdn.discordapp.com/attachments/842273685422604328/860762192173072424/flashbang_lol.mp3")).play(); // lol
+console.log("Corrupted " + DESTROYER.COUNT + " DOM properties.")
