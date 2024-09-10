dd4b1ffd5d91cf48141ad8f94771587c9fd3c1a5
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2024-09-10 16:07:21 -0400

    v3

--- chrome-searcher.js
+++ chrome-searcher.js
@@ -1,174 +1,187 @@
 /************************************
-*     Fast's JS Global Searcher     *
+*     Fast's Global Object Search   *
 *      Chrome Console Edition       *
 ************************************/
-// version 2
-var searcher = { 
-    isCursedKey: (key) => key.match(/[^(a-z|$|_|A-Z)]/), 
-    justify: (string,value) => (string + "\t".repeat(Math.max(0,Math.ceil((value-string.length)/4)))),
-    isBanned: (key) => key == "window.searcher" || searcher.includesPartial(searcher.BANNED_KEYS,key), // pls don't remove hardcoded values from here
-};
 
 /*  HOW TO USE:
     Paste in chrome console and hit enter.  Take care if you set the max search depth really high.
 */
-searcher.BANNED_KEYS         = ["document"]; // traversal will stop at these keys
-searcher.SEARCH_KEYS         = ["invite"];   // keys or values containing these strings will be displayed
-searcher.MAX_SEARCH_DEPTH    = 10;           // max recursive depth to search
 
-searcher.LIST_GROUPING_MODE = console.groupCollapsed  // can be changed to console.group, but not reccommended
+const SEARCHER = { 
 
-searcher.COLORS = {  // inspired by Wiremod's Expression 2 Language in Garry's Mod
-    string:    "color: #999999;",
-    number:    "color: #ff6666;",
-    bigint:    "color: #a45b5b;",
-    boolean:   "color: #668cff;",
-    symbol:    "color: #fbfb51;",
-    object:    "color: #80ff80;",
-    array:     "color: #80ff80;",
-    undefined: "color: #ffb56b;",
-    function:  "color: #fc83fc;",
-};
+    isCursedKey: (key) => key.match(/[^(a-z|$|_|A-Z)]/), 
+    justify:     (string,value) => (string + "\t".repeat(Math.max(0,Math.ceil((value-string.length)/4)))),
 
-searcher.getPath = function(path,key){
-    if( searcher.isCursedKey(key) ){
-        if( parseInt(key) != NaN ){ // int
-            return `${path}[${key}]`
-        }
-        else{ // something nasty
-            return `${path}[\`${key}\`]`
-        }
-    }
-    else{
-        return `${path}.${key}`;
-    }
-}
+    isBanned(key){ return key == "window.SEARCHER" || this.includesPartial(this.BANNED_KEYS,key) }, // pls don't remove hardcoded values from here
 
-/**
- * @param {String} string 
- * @returns 
- */
-searcher.includesPartial = function(strings,string){
-    for (const key in strings) {
-        if(string.toLowerCase().includes(strings[key])){ return true; }
-    }
-    return false;
-};
+    COLORS: {  
+        // inspired by Wiremod's Expression 2 Language in Garry's Mod
+        string:    "color: #999999;",
+        number:    "color: #ff6666;",
+        bigint:    "color: #a45b5b;",
+        boolean:   "color: #668cff;",
+        symbol:    "color: #fbfb51;",
+        object:    "color: #80ff80;",
+        array:     "color: #80ff80;",
+        undefined: "color: #ffb56b;",
+        function:  "color: #fc83fc;",
+    },
 
-searcher.advToString = function(thing){
-    let type = typeof(thing);
-    let value = "failed..."
-    try{
-        switch(type){
-            case 'symbol': value = thing.valueOf(); break; // ugh
-            case 'string': value = `"${thing}"`; break;
-            case 'function': 
-                value = (thing + "").match(/(function\s*)([^\)]*\(.+)/); 
-                if(value){ value = value[2] }
-                break;
-            case 'object':
-                value = thing + ""
-                if(!value.startsWith("[")){
-                    value = `[...]`
-                    type = "array";
-                }
-            break;
-            default: value = thing + "";
-        }
-    }
-    finally{}
-    return value;
-}
+    BANNED_KEYS: ["document"], // traversal will stop at these keys; this is semi-hardcoded for your own good.
 
-// call this on any value and leave location blank to print advanced data about it
-searcher.advLog = function(thing,path){
-    let type = typeof(thing);
-    let value = "failed..."
-    try{
-        switch(type){
-            case 'symbol': value = thing.valueOf(); break; // ugh
-            case 'string': value = `"${thing}"`; break;
-            case 'function': 
-                value = (thing + "").match(/(function\s*)([^\)]*\(.+)/); 
-                if(value){ value = value[2] }
-                break;
-            case 'object':
-                value = thing + ""
-                if(!value.startsWith("[")){
-                    value = `[...]`
-                    type = "array";
-                }
-            break;
-            default: value = thing + "";
-        }
-    }
-    finally{}
-    if(path){
-        console.log( `${path} = %c${type} %c${value}`, "color: #ff944d;", searcher.COLORS[type] )
-    }
-    else{
-        console.log( `%c${type} %c${value}`, "color: #ff944d;", searcher.COLORS[type] )
-    }
-}
+    /**
+     * @param {String} path - current path
+     * @param {String} key  - key to add 
+     * @returns {String} new path
+     */
+    getPath(path, key){
+        return (typeof key === 'symbol') ? 
+            `${path}[${key.toString()}]`
+            : key.toString().match(/[^(a-z|$|_|A-Z)]/) ? 
+            `${path}[\`${key.toString()}\`]` // evil string key or some other bullshit
+            : `${path}.${key}`;              // normal key
+    },
 
-searcher.recurse = function(obj, path, depth=0, refs=new WeakSet(), funcs=[]) {    
-
-    if( depth > searcher.MAX_SEARCH_DEPTH ){ return; }
-
-    // Avoid infinite recursion
-    if(refs.has(obj)){ return; }
-    else if( obj!=null ){ refs.add(obj); }
-
-    bruh: for (const key in obj) {
+    /**
+     * better toString that actually tells you what it is specifically if it's not a string
+     * @param {Object} thing
+     * @returns {String}
+     */
+    advToString(thing){
+        let type = typeof(thing);
+        let value = "failed..."
         try{
-
-            let value = obj[key]; // if we hit a css sheet this will throw an exception
-            if( value === null || value === undefined ){ continue; } // skip null
-            if( value && value.window == window ){ continue; } // no.
-            let type = typeof(value);
-            let newpath = searcher.getPath(path,key);
-            if( searcher.isBanned(newpath) ){ 
-                continue bruh; 
-            }
-
             switch(type){
-                case 'function':
-                    let code = (value+"")
-                    if( code.includes("[native code]") ){
-                        continue bruh;
-                    }
-                    if( searcher.includesPartial(searcher.SEARCH_KEYS,code) ){
-                        funcs.push(value);
-                        searcher.advLog(value, newpath)
-                        continue bruh;
-                    }
+                case 'symbol': value = thing.description; break; // ugh
+                case 'string': value = `"${thing}"`; break;
+                case 'function': 
+                    value = (thing + "").match(/(function\s*)([^\)]*\(.+)/); 
+                    if(value){ value = value[2] }
+                    break;
                 case 'object':
-                    if( searcher.includesPartial(searcher.SEARCH_KEYS,key) ){
-                        searcher.advLog(value, newpath)
-                        continue bruh;
-                    }
-                    searcher.recurse(value, newpath, depth+1, refs, funcs);          
-                break;
-                default:
-                    if( searcher.includesPartial(searcher.SEARCH_KEYS,key) ){
-                        searcher.advLog(value, newpath)
-                        continue bruh;
-                    }
-                    let text = searcher.advToString(value)
-                    if( searcher.includesPartial(searcher.SEARCH_KEYS,text) ){
-                        searcher.advLog(value, newpath)
-                        continue bruh;
+                    value = thing + ""
+                    if(!value.startsWith("[")){
+                        value = `[...]`
+                        type = "array";
                     }
                 break;
+                default: value = thing + "";
             }
+        }
+        finally{}
+        return value;
+    },
 
+    /**
+     * @param {String} keywords - search keywords
+     * @param {Object} string  - victim
+     * @returns {boolean} did any keyword match?
+     */
+    includesPartial(keywords, string){
+        try {
+            const lowerKeywords = keywords.map( s => s.toLowerCase() );
+            let lowerString = this.advToString(string);
+            lowerString = lowerString+"";
+            for( let lowerKeyword of lowerKeywords ){
+                if( lowerString.includes(lowerKeyword) ){ return true; }
+            }
         }
         catch(e){
-            console.error(e);
+            debugger;
         }
+
+
+        return false;
+    },
+
+    /**
+     * prints a thing at a path to console
+     * @param {Object} thing
+     * @param {String} path
+     */
+    advLog(thing, path, stuff=[]){
+        let id = stuff.push(thing);
+
+        let type = typeof(thing);
+        let value = "failed..."
+        try{
+            switch(type){
+                case 'symbol': value = thing.valueOf(); break; // ugh
+                case 'string': value = `"${thing}"`; break;
+                case 'function': 
+                    value = (thing + "").match(/(function\s*)([^\)]*\(.+)/); 
+                    if(value){ value = value[2] }
+                    break;
+                case 'object':
+                    value = thing + ""
+                    if(!value.startsWith("[")){
+                        value = `[...]`
+                        type = "array";
+                    }
+                break;
+                default: value = thing + "";
+            }
+        }
+        finally{}
+        if(path){
+            console.log( `(${id}) ${path} = %c${type} %c${value}`, "color: #ff944d;", this.COLORS[type] )
+        }
+    },
+
+    search(obj, SEARCH_KEYS=[], MAX_SEARCH_DEPTH=10, path="", depth=0, refs=new WeakSet(), stuff=[]) {
+
+        if( depth > MAX_SEARCH_DEPTH ){ return; }
+    
+        // Avoid infinite recursion
+        if(refs.has(obj)){ return; }
+        else if( obj!=null ){ refs.add(obj); }
+    
+        bruh: for (const key in obj) {
+            try{
+    
+                let value = obj[key]; // if we hit a css sheet this will throw an exception
+                if( value === null || value === undefined ){ continue; } // skip null
+                if( value && value.window == window ){ continue; } // no.
+                let type = typeof(value);
+                let newpath = this.getPath(path, key);
+                if( this.isBanned(newpath) ){ 
+                    continue bruh; 
+                }
+    
+                switch(type){
+                    case 'function':
+                        let code = (value+"")
+                        if( this.includesPartial(SEARCH_KEYS, code) ){
+                            this.advLog(value, newpath, stuff)
+                        }
+                    case 'object':
+                        if( this.includesPartial(SEARCH_KEYS, value.constructor.name) ){
+                            this.advLog(value, newpath, stuff)
+                        }
+                        this.search(value, SEARCH_KEYS, MAX_SEARCH_DEPTH, newpath, depth+1, refs, stuff);          
+                    break;
+                }
+    
+                if( this.includesPartial(SEARCH_KEYS, key) ){
+                    this.advLog(value, newpath, stuff)
+                    continue bruh;
+                }
+                
+                let text = this.advToString(value)
+                if( this.includesPartial(SEARCH_KEYS, text) ){
+                    this.advLog(value, newpath, stuff)
+                    continue bruh;
+                }
+    
+            }
+            catch(e){
+                console.error(e);
+            }
+        }
+
+        return stuff;
+    
     }
 
-    return funcs
-
 };
-searcher.recurse(window,"window")
+
